77facff87485dd910ec30c15372957e7bca9b254
user-kk <2908065055@qq.com>
2022-11-09 21:23:47 +0800

    左侧列表框完成

--- src/main/java/com/example/MyFrame.java
+++ src/main/java/com/example/MyFrame.java
@@ -4,21 +4,27 @@ import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
 public class MyFrame extends JFrame {
-    private static final int DEFAULT_WIDTH=1200;
-    private  static  final  int DEFAULT_HEIGHT=800;
 
-    private DefaultListModel<String> model=new DefaultListModel<>();//左侧列表的内容
-    public MyFrame(){
+    private Boolean toolBarVisible = false;
+
+    private MyJList jlist=new MyJList();
+    public MyFrame() {
         init();
     }
-    private void init (){
-        this.setSize(DEFAULT_WIDTH,DEFAULT_HEIGHT);
+
+    private void init() {
+        //初始化
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        this.setLocation(0, 0);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("MyPPT");
+
+
+        //设置菜单栏
         JPanel panel = new JPanel(new BorderLayout());
         JMenuBar jMenuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("文件");
@@ -37,21 +43,18 @@ public class MyFrame extends JFrame {
         fileMenu.add(jMenuItemSaveE);
         fileMenu.addSeparator();
         fileMenu.add(jMenuItemBack);
-
-
         jMenuBar.add(fileMenu);
         jMenuBar.add(toolMenu);
         this.setJMenuBar(jMenuBar);
 
 
-
+        //设置工具栏
         JButton button = new JButton("画笔");
         JButton button1 = new JButton("长方形");
         JButton button2 = new JButton("添加文字");
         JButton button3 = new JButton("橡皮檫");
         JButton button4 = new JButton("椭圆");
         JButton button5 = new JButton("直线");
-
         final JToolBar jToolBar = new JToolBar();
         jToolBar.setFloatable(false);
         jToolBar.add(button);
@@ -61,15 +64,22 @@ public class MyFrame extends JFrame {
         jToolBar.add(button4);
         jToolBar.add(button5);
         jToolBar.setVisible(false);
+
+
+        //绑定菜单栏事件
         toolMenu.addMenuListener(new MenuListener() {
             @Override
             public void menuSelected(MenuEvent e) {
-                jToolBar.setVisible(true);
+                toolBarVisible = !toolBarVisible;
+                jToolBar.setVisible(toolBarVisible);
+
             }
+
             @Override
             public void menuDeselected(MenuEvent e) {
 
             }
+
             @Override
             public void menuCanceled(MenuEvent e) {
             }
@@ -78,6 +88,7 @@ public class MyFrame extends JFrame {
             @Override
             public void menuSelected(MenuEvent e) {
                 jToolBar.setVisible(false);
+                toolBarVisible = false;
             }
 
             @Override
@@ -91,32 +102,24 @@ public class MyFrame extends JFrame {
             }
         });
 
+        //设置左侧
 
+        JScrollPane jScrollPane = new JScrollPane(jlist, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
-
-
-        //设置左侧列表
-        for(int i=0;i<3;++i){
-            model.addElement(String.valueOf(i));
-        }
-        JList<String> jlist=new JList<>(model);
-        JScrollPane jScrollPane = new JScrollPane(jlist);
-        jlist.setFixedCellHeight(250);
-        jlist.setFixedCellWidth(250);
-        DefaultListCellRenderer renderer = new DefaultListCellRenderer();
-        renderer.setHorizontalAlignment(SwingConstants.CENTER);
-        jlist.setCellRenderer(renderer);
 
         //设置中间
         Panel centerPanel = new Panel();
         //设置右侧
         Panel rightPanel = new Panel();
 
-        panel.add(centerPanel,BorderLayout.CENTER);
-        panel.add(rightPanel,BorderLayout.EAST);
-        panel.add(jScrollPane,BorderLayout.WEST);
+        //设置布局
+        panel.add(centerPanel, BorderLayout.CENTER);
+        panel.add(rightPanel, BorderLayout.EAST);
+        panel.add(jScrollPane, BorderLayout.WEST);
         panel.add(jToolBar, BorderLayout.NORTH);
         this.add(panel);
         this.setVisible(true);
+
     }
 }
--- src/main/java/com/example/MyWindow.java
+++ src/main/java/com/example/MyWindow.java
@@ -1,8 +1,8 @@
 package com.example;
 
-import com.example.MyFrame;
 
 public class MyWindow {
+
     public static void main(String[] args) {
         MyFrame myFrame = new MyFrame();
 
