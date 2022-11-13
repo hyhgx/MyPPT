d76bebb3c857d0f1e31a142827312bfc16f342fd
user-kk <2908065055@qq.com>
2022-11-13 20:18:05 +0800

    添加照片功能(未完成)

---
+++ src/main/java/com/example/MyImage.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.awt.*;
+
+public class MyImage extends MyComponent2D{
+
+    private final Image image;
+    public MyImage(int x,int y ,Image image){
+        super(x,y);
+        this.image=image;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+       // g.drawImage()
+        //g.drawImage(5,5,maxX-minX,maxY-minY)
+    }
+}
